ccbfac633db054bec5c3e4d96ce1641d0cec7e2d
RakibHossen678 <rh491464@gmail.com>
2024-03-16 12:39:25 +0600

    added functionality on button

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import './App.css'
-import Banner from './Components/Banner/Banner'
+import { useState } from "react";
+import "./App.css";
+import Banner from "./Components/Banner/Banner";
 
-import Details from './Components/Details/Details'
-import Navbar from './Components/Navbar/Navbar'
-import Recipe from './Components/Recipe/Recipe'
-import Recipes from './Components/Recipes/Recipes'
+import Details from "./Components/Details/Details";
+import Navbar from "./Components/Navbar/Navbar";
+import Recipe from "./Components/Recipe/Recipe";
+import Recipes from "./Components/Recipes/Recipes";
 
 function App() {
-  
-  const [Cart,setCart]=useState([])
-  const handleCook=(cart)=>{
-    setCart([...Cart,cart])
-  }
-  console.log(Cart)
+  const [Cart, setCart] = useState([]);
+  const handleCook = (cart) => {
+    const duplicate = Cart.find((c) => c.recipe_id === cart.recipe_id);
+    // console.log(duplicate)
+    if (!duplicate) {
+      setCart([...Cart, cart]);
+    }
+    
+  };
+  console.log(Cart);
 
   return (
-    <div className='lg:w-10/12 w-11/12 mx-auto py-5'>
+    <div className="lg:w-10/12 w-11/12 mx-auto py-5">
       <Navbar></Navbar>
       <Banner></Banner>
       <Recipes></Recipes>
-      <div className='flex flex-col lg:flex-row justify-between'>
-        <div className='lg:w-3/4'>
+      <div className="flex flex-col lg:flex-row justify-between">
+        <div className="lg:w-3/4">
           <Recipe handleCook={handleCook}></Recipe>
         </div>
-        <div className='lg:w-5/12'>
-          <Details></Details>
+        <div className="lg:w-5/12">
+          <Details Cart={Cart}></Details>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Details/Details.jsx
+++ src/Components/Details/Details.jsx
@@ -1,11 +1,12 @@
-const Details = () => {
+const Details = ({Cart}) => {
   return (
     <div className="mt-16 border-2  ">
+      
       <div className="">
         <h1 className="text-center text-lg font-medium border-b-2 pb-2 border-gray-300">
           Want to cook: 01
         </h1>
-        <table>
+        {/* <table>
           <tr className=" lg:space-x-20 space-x-16">
             <th></th>
             <th>Name</th>
@@ -14,6 +15,7 @@ const Details = () => {
             <th></th>
           </tr>
           <tr className=" ">
+            
             <td>1</td>
             <td className="lg:pr-12 pr-10 pl-3 max-w-4">Alfreds Futterkiste</td>
             <td className="lg:px-14 px-10 max-w-4">Maria Anders</td>
@@ -23,28 +25,44 @@ const Details = () => {
               bg-[#0BE58A] px-4 font-medium py-1 rounded-full">Prepared</button>
             </td>
           </tr>
-        </table>
-        {/* <div className="px-2">
+        </table> */}
+        <div className="px-2">
           <div className="flex  pt-2 pb-4 font-medium text-gray-500">
             <div className="flex lg:space-x-20 space-x-16">
               <p> Name</p>
-              <p>Time</p>
-              <p>Calories</p>
+              <p className="pl-2">Time</p>
+              <p className="">Calories</p>
             </div>
           </div>
-          <div className="flex justify-between pb-3">
-            <div className="flex lg:space-x-20 space-x-16">
-              <p>
-                <span className="pr-2">1</span>Name
-              </p>
-              <p>Time</p>
-              <p>Calories</p>
-            </div>
-            <div>
-              <button className="">Prepare</button>
-            </div>
+          <div className="  pb-3">
+            
+              {
+                Cart.map(cart=>(
+                  <div key={cart.recipe_id} className="flex lg:space-x-10 space-x-16 pb-4">
+                  <p >
+                  <span  className="pr-1 max-w-5">{cart.recipe_id}.</span>{cart.recipe_name}
+                  </p>
+                  <p className="">
+                    <h1>{cart.preparing_time}</h1>
+                    <p>minutes</p>
+                  </p>
+                  <p>
+                  <h1>{cart.calories}</h1>
+                    <p>calories</p>
+                  </p>
+                  <div>
+                    <button className="
+              bg-[#0BE58A] px-4 font-medium py-1 rounded-full">Prepared</button>
+                  </div>
+                 </div>
+
+
+                ))
+              } 
+            
+            
           </div>
-        </div> */}
+        </div>
       </div>
     </div>
   );
